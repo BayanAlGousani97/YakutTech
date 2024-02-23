4a76e58b1a981dc20bf1d9ef40b48a655504975c
bayanalgousani <bayanalgousani@gmail.com>
2024-02-23 23:33:41 +0200

    edit path to dashboard

--- routes/web.php
+++ routes/web.php
@@ -18,14 +18,15 @@ use Illuminate\Support\Facades\Route;
 */
 
 
-//Route::get('/dashboard', function () {
-//    return view('dashboard');
-//})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::middleware('auth')->prefix('admin')->group(function () {
+
+Route::middleware(['auth', 'verified'])->prefix('admin')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
 });
 
 require __DIR__ . '/auth.php';
